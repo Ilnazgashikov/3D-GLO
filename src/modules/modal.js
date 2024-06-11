1907c076f43c646367bf963d40a71c818a9a1123
Ilnazgashikov <tastycrazyfood@gmail.com>
2024-06-12 00:53:42 +0300

    lesson20

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,34 +2,40 @@ const modal = () => {
     const buttons = document.querySelectorAll('.popup-btn')
     const modal = document.querySelector('.popup')
     const modalContent = modal.querySelector('.popup-content')
-    modalContent.style.transition = 'all 1s'
-    modal.style.transition = 'all 1s'
-    const closeBtn = modal.querySelector('.popup-close')
+    modalContent.style.transition = 'all .5s'
     modalContent.style.transform = 'translateY(100%)'
-    modal.style.opacity = '0'
-    let screenSmaller = false
+    modalContent.style.transform = 'translateX(0)'
+    modal.style.transition = 'all .5s'
+
+    const closeBtn = modal.querySelector('.popup-close')
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            if (!screenSmaller) {
+            if (window.screen.width >= 768) {
                 modal.style.display = 'block'
                 setTimeout(() => {
                     modalContent.style.transform = 'translateY(50%)'
                     modal.style.opacity = '1'
                 }, 0)
             }
+            else {
+                modal.style.display = 'block'
+                modal.style.opacity = '1'
+                modalContent.style.transform = 'translateY(50%)'
+            }
         });
     })
     closeBtn.addEventListener('click', () => {
-        if (!screenSmaller) {
+        if (window.screen.width >= 768) {
             modalContent.style.transform = 'translateY(100%)'
             modal.style.opacity = '0'
             setTimeout(() => {
                 modal.style.display = 'none'
-            }, 1000)
+            }, 500)
+        }
+        else {
+            modal.style.display = 'none'
+            modal.style.opacity = '0'
         }
     })
-    window.addEventListener('resize', (event) => {
-        window.screen.width <= 768? screenSmaller = true :  screenSmaller = false
-    }, true);
 }
 export default modal

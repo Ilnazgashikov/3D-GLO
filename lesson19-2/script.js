e263fd45ef0af8cf95997b56f9d069dd26f4d408
Ilnazgashikov <tastycrazyfood@gmail.com>
2024-06-11 20:19:53 +0300

    lesson19

--- lesson19-2/script.js
+++ lesson19-2/script.js
@@ -1,3 +1,7 @@
+const newYearText = document.getElementById('before-new-year');
+const textHello = document.getElementById('text-hello');
+const dayOfWeek = document.getElementById('day-of-week');
+const timeAmPm = document.getElementById('time-am-pm');
 let dateNow = new Date();
 const beforeNewYear = () => {
     let dateStop = new Date('31 december 2024');
@@ -6,21 +10,40 @@ const beforeNewYear = () => {
     if (timeRemaining < 0) {
         dateStop.setFullYear(dateStop.getFullYear() + 1)
     }  
-    console.log(`До нового года осталось ${days} дней`)
+    newYearText.textContent = `До нового года осталось ${days} дней`
 }
 const timeOfDay = () => {
     let hour = dateNow.getHours();
     if (hour <= 6) {
-        console.log('Доброе время суток');
+       textHello.textContent = 'Доброе время суток';
     } else if (hour <= 12) {
-        console.log('Доброе утро');
+        textHello.textContent = 'Доброе утро';
     } else if (hour <= 18) {
-        console.log('Добрый день');
+        textHello.textContent = 'Добрый день';
     } else {
-        console.log('Добрый вечер');
+        textHello.textContent = 'Добрый вечер';
     }
 }
-timeOfDay();
+const getWeekDay = () => {
+    let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    dayOfWeek.textContent = `Сегодня: ${days[dateNow.getDay()]}`;
+}
+
+const getAmPmHours = () => {
+    let date = new Date();
+    let hours = date.getHours() % 12;
+    let minutes = ('0' + date.getMinutes()).slice(-2); 
+    let secundes = date.getSeconds();
+    if (secundes < 10) {
+        secundes = `0${secundes}`
+    } 
+    let ampm = date.getHours() >= 12 ? 'PM' : 'AM';
+    timeAmPm.textContent = `${hours}:${minutes}:${secundes} ${ampm}`;
+}
+timeOfDay()
+getWeekDay()
+setInterval(getAmPmHours, 1000)
+getAmPmHours();
 beforeNewYear()
 
 

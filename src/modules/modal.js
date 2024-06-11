fe35ba5ab39502a491459b07845828bf75cb9478
Ilnazgashikov <tastycrazyfood@gmail.com>
2024-06-11 22:33:34 +0300

    lesson20

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -3,9 +3,10 @@ const modal = () => {
     const modal = document.querySelector('.popup')
     const modalContent = modal.querySelector('.popup-content')
     modalContent.style.transition = 'all 1s'
+    modal.style.transition = 'all 1s'
     const closeBtn = modal.querySelector('.popup-close')
     modalContent.style.transform = 'translateY(100%)'
-    modalContent.style.opacity = '0'
+    modal.style.opacity = '0'
     let screenSmaller = false
     buttons.forEach(button => {
         button.addEventListener('click', () => {
@@ -13,7 +14,7 @@ const modal = () => {
                 modal.style.display = 'block'
                 setTimeout(() => {
                     modalContent.style.transform = 'translateY(50%)'
-                    modalContent.style.opacity = '1'
+                    modal.style.opacity = '1'
                 }, 0)
             }
         });
@@ -21,7 +22,7 @@ const modal = () => {
     closeBtn.addEventListener('click', () => {
         if (!screenSmaller) {
             modalContent.style.transform = 'translateY(100%)'
-            modalContent.style.opacity = '0'
+            modal.style.opacity = '0'
             setTimeout(() => {
                 modal.style.display = 'none'
             }, 1000)

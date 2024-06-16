ef79c8eabc3d1c8adc8987dae8958dd0d92feced
Ilnazgashikov <tastycrazyfood@gmail.com>
2024-06-16 18:48:09 +0300

    lesson21

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import timer from "./modules/timer.js";
 import menu from "./modules/menu.js";
 import modal from "./modules/modal.js";
+import validator from "./modules/validator.js";
 timer("15 june 2024");
 menu()
 modal()
+validator()

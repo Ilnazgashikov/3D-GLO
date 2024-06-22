65c64e2553e56c98746a367a65f60febd18812c5
Ilnazgashikov <tastycrazyfood@gmail.com>
2024-06-22 16:52:36 +0300

    lesson24

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import menu from "./modules/menu.js";
 import modal from "./modules/modal.js";
 import validator from "./modules/validator.js";
 import slider from "./modules/slider.js";
+import calc from "./modules/calc.js";
 timer("25 june 2024");
 menu()
 modal()
 validator()
 slider()
+calc(100)
